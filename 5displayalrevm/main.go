25d524c1bd80fbb57c224cc0efaf17a18d4ececd
rshezarr <rshezarr@gmail.com>
2022-05-17 07:29:37 +0600

    alem

--- 5displayalrevm/main.go
+++ 5displayalrevm/main.go
@@ -2,11 +2,25 @@ package main
 
 import "github.com/01-edu/z01"
 
-func main() {
-	a := "zYxWvUtSrQpOnMlKjIhGfEdCbA"
+// func main() {
+// 	a := "zYxWvUtSrQpOnMlKjIhGfEdCbA"
 
-	for _, w := range a {
-		z01.PrintRune(w)
+// 	for _, w := range a {
+// 		z01.PrintRune(w)
+// 	}
+// 	z01.PrintRune('\n')
+// }
+
+// OR
+
+func main() {
+	for i := 'z'; i >= 'a'; i-- {
+		if i%2 == 1 {
+			n := i - 32
+			z01.PrintRune(n)
+		} else {
+			z01.PrintRune(i)
+		}
 	}
 	z01.PrintRune('\n')
 }
--- 6displayalpham/main.go
+++ 6displayalpham/main.go
@@ -2,11 +2,25 @@ package main
 
 import "github.com/01-edu/z01"
 
-func main() {
-	a := "aBcDeFgHiJkLmNoPqRsTuVwXyZ"
+// func main() {
+// 	a := "aBcDeFgHiJkLmNoPqRsTuVwXyZ"
 
-	for _, w := range a {
-		z01.PrintRune(w)
+// 	for _, w := range a {
+// 		z01.PrintRune(w)
+// 	}
+// 	z01.PrintRune('\n')
+// }
+
+// OR
+
+func main() {
+	for i := 'a'; i <= 'z'; i++ {
+		if i%2 == 0 {
+			n := i - 32
+			z01.PrintRune(n)
+		} else {
+			z01.PrintRune(i)
+		}
 	}
 	z01.PrintRune('\n')
 }
